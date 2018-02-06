95cc3167415b4dfd97538d7dd77d62fc3cb8fecb
Manh Tan Nguyen <tan.mng90@gmail.com>
2018-02-06 11:03:06 -0500

    Some correct of profile list from config file

--- completions/asp.fish
+++ completions/asp.fish
@@ -2,7 +2,7 @@ function __fish_print_aws_profiles -d 'Print a list of AWS profiles' -a 'select'
     set -x CONFIG_FILE ~/.aws/config
     set -x CREDENTIAL_FILE ~/.aws/credentials
 
-    command sed -n 's/^\[(?profile )?\(.*\)\]/\1/p' $CONFIG_FILE
+    command sed -n 's/^ *\[\(.*\)\] *$/\1/p' ~/.aws/config | grep -Eo '[^ ]+$'
 end
 
 complete --command asp --no-files --arguments '(__fish_print_aws_profiles)'
